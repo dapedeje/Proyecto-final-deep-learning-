01e4e53e4e641b63e4eaee9925a4d5a43f24197a
dapedeje <dapedeje@amni.uv.es>
2026-03-03 11:23:46 +0100

    Se ha credo el modelo simple

---
+++ modelos/RNNMoreSimple.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Flatten, GlobalAveragePooling2D
+from keras.optimizers import Adam
+
+def ModeloSimpleCLS(input_shape, num_clases):
+    """
+    Esta función permite cargar una arquitectura simple para un modelo CNN.
+
+    Args:
+        input_shape (_type_): Insertar el tamaño de la entrada para el modelo
+    """
+    model = Sequential()
+    
+    model.add(Conv2D(num_clases, kernel_size= 3, activation='relu', input_shape=input_shape))
+    model.add(GlobalAveragePooling2D())
+
+    optimizer_adam = Adam()
+    model.compile(optimizer=optimizer_adam, loss='sparse_categorical_crossentropy', metrics=['categorical_accuracy'])
+    
+    return model
