ace6ad5f6d8e11f006f2b3d9cf9b2f0607fc5378
dapedeje <dapedeje@alumni.uv.es>
2026-03-11 12:38:05 +0100

    Subo los ficheros para un modelo complejo nuevo

---
+++ modelos/CNNComplejo2.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Flatten
+from keras.optimizers import Adam
+
+def ModeloComplejo(input_shape, num_clases):
+    """
+    Esta función permite crear una arquitectura más compleja que la entrega 
+    anterior
+
+
+    Args:
+        input_shape (_type_): Tamaño de la entrada
+        num_clases (_type_): numero de clases a predecir
+
+    Returns:
+        _type_: modelo de keras
+    """
+    model = Sequential()
+    
+    model.add(Conv2D(32, kernel_size= 3, activation='relu', input_shape=input_shape))
+    model.add(Conv2D(64, 3, activation= 'relu')) #Añadimos complejidad
+    
+    model.add(Flatten())
+    model.add(Dense(64, activation= 'relu'))#Añadimos complejidad
+    model.add(Dense(num_clases, activation='softmax'))#Añadimos complejidad
+    
+    optimizer_adam = Adam()
+    model.compile(optimizer=optimizer_adam, loss='sparse_categorical_crossentropy', metrics=['categorical_accuracy'])
+    
+    return model
