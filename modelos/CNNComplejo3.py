dac00ab45b13d4ca542c0248f5059cd26e1c4111
dapedeje <dapedeje@alumni.uv.es>
2026-03-12 13:21:30 +0100

    Creado el modelo complejo 3

--- modelos/CNNComplejo3.py
+++ modelos/CNNComplejo3.py
@@ -19,27 +19,23 @@ def ModeloComplejo(input_shape, num_clases):
     model = Sequential()
     
     model.add(layers.Conv2D(32, 3, padding='same', input_shape=input_shape))
-    model.add(layers.BatchNormalization())
+    #model.add(layers.BatchNormalization())
     model.add(layers.ReLU())
     model.add(layers.MaxPooling2D((2,2)))
-    model.add(layers.Dropout(0.2))
 
     model.add(layers.Conv2D(64, 3, padding='same', input_shape=input_shape))
-    model.add(layers.BatchNormalization())
+    #model.add(layers.BatchNormalization())
     model.add(layers.ReLU())
     model.add(layers.MaxPooling2D((2,2)))
-    model.add(layers.Dropout(0.2))
     
 
     model.add(layers.Conv2D(128, 3, padding='same', input_shape=input_shape))
-    model.add(layers.BatchNormalization())
+    #model.add(layers.BatchNormalization())
     model.add(layers.ReLU())
     model.add(layers.MaxPooling2D((2,2)))
-    model.add(layers.Dropout(0.2))
     
     model.add(layers.Flatten())
     model.add(layers.Dense(256, activation='relu'))
-    model.add(layers.Dropout(0.5))
     model.add(layers.Dense(num_clases, activation='softmax'))
     
     optimizer_adam = Adam()
