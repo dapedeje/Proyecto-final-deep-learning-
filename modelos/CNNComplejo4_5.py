3f694304526b0ac40b4eead46585165874d7825d
dapedeje <dapedeje@alumni.uv.es>
2026-03-13 16:36:51 +0100

    Quite una capa de dropout porque no esta aprendiendo

--- modelos/CNNComplejo4_5.py
+++ modelos/CNNComplejo4_5.py
@@ -36,7 +36,6 @@ def ModeloComplejo(input_shape, num_clases):
     
     model.add(layers.Flatten())
     model.add(layers.Dense(32, activation='relu'))
-    model.add(layers.Dropout(0.2))
     model.add(layers.Dense(64, activation='relu'))
     model.add(layers.Dropout(0.2))
     model.add(layers.Dense(256, activation='relu'))
