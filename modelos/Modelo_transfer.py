fa69bacb5f1d310f8ecf8ee70c62557a8c5b0096
dapedeje <dapedeje@amni.uv.es>
2026-03-14 16:03:14 +0100

    Subo los ficheros para adaptar el código

---
+++ modelos/Modelo_transfer.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+from keras.models import Sequential
+from tensorflow.keras import layers, models
+from keras.optimizers import Adam
+from tensorflow.keras.applications import ResNet50
+from keras.layers import GlobalAveragePooling2D, Dense
+from keras import Model
+
+def Modelo_transfer(input_shape, num_clases):
+    """
+    Esta función permite crear una arquitectura más compleja que la entrega 
+    anterior
+
+
+    Args:
+        input_shape (_type_): Tamaño de la entrada
+        num_clases (_type_): numero de clases a predecir
+
+    Returns:
+        _type_: modelo de keras
+    """
+    base_model = ResNet50(weights="imagenet", include_top=False)
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    predictions = Dense(num_clases, activation='softmax')(x)
+    model = Model(inputs=base_model.input, outputs=predictions)
+    
+    for layer in base_model.layers:
+        layer.trainable = False
+
+
+    optimizer_adam = Adam()
+    model.compile(optimizer=optimizer_adam, loss='sparse_categorical_crossentropy', metrics=['sparse_categorical_accuracy'])
+    
+    return model
