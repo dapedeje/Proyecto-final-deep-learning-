b3cb4e764243ab1ee64df50a62283dcf8c97e326
dapedeje <dapedeje@amni.uv.es>
2026-03-14 16:38:08 +0100

    Arreglo del modelo

--- modelos/Modelo_transfer.py
+++ modelos/Modelo_transfer.py
@@ -22,6 +22,11 @@ def Modelo_transfer(input_shape, num_clases):
     base_model = ResNet50(weights="imagenet", include_top=False)
     x = base_model.output
     x = GlobalAveragePooling2D()(x)
+    x = Dense(256,activation='relu')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.ReLU()(x)
+    x = layers.Dropout(0.3)(x)
+
     predictions = Dense(num_clases, activation='softmax')(x)
     model = Model(inputs=base_model.input, outputs=predictions)
     
